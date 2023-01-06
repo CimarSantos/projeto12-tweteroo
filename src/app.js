5c43a23761b5129b8a6d906a8af4ecd0594cdf25
CimarSantos <francimarlsantos@outlook.com>
2023-01-06 12:47:49 -0300

    Feat: Projeto base com requisitos gerais

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 5000;
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, console.log(`Tweeteroo running on port ${PORT}`));
