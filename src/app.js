44870761df7af024c3aa1a66d5dbfb65174dd744
CimarSantos <francimarlsantos@outlook.com>
2023-01-06 13:15:14 -0300

    Feat: Verbo POST para a rota sign-up com status 200

--- src/app.js
+++ src/app.js
@@ -6,4 +6,12 @@ const PORT = 5000;
 app.use(express.json());
 app.use(cors());
 
+const users = [];
+
+app.post("/sign-up", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send("ok");
+});
+
 app.listen(PORT, console.log(`Tweeteroo running on port ${PORT}`));
