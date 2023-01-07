00ad97d2267124e87c334a43cfa29e15fec59674
CimarSantos <francimarlsantos@outlook.com>
2023-01-07 19:04:52 -0300

    Feat: POST para os tweets com verificação de sign-up do usuário

--- src/app.js
+++ src/app.js
@@ -15,10 +15,26 @@ app.post("/sign-up", (req, res) => {
   res.send("ok");
 });
 
+app.get("/sign-up", (req, res) => {
+  const username = req.body.username;
+  res.send(users);
+  console.log(username);
+});
+
 app.post("/tweets", (req, res) => {
+  const username = req.body.username;
+  const signedUser = users.find((user) => user.username === username);
+  if (!signedUser) {
+    res.send("UNAUTHORIZED");
+    return;
+  }
   const tweet = req.body;
   tweets.push(tweet);
   res.send("ok");
+});
+
+app.get("/tweets", (_, res) => {
+  res.send(tweets);
 });
 
 app.listen(PORT, console.log(`Tweeteroo running on port ${PORT}`));
